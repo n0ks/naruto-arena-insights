8bf79b05179238e874ab063f5ddb493859a0b70b
Rodrigo Martins <rodrigo.m.de.campos@accenture.com>
2019-09-20 17:30:31 -0300

    feat: android configs

--- android/app/src/main/java/com/narutoarenaapp/MainActivity.java
+++ android/app/src/main/java/com/narutoarenaapp/MainActivity.java
@@ -1,11 +1,11 @@
 package com.narutoarenaapp;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle; // here
+import org.devio.rn.splashscreen.SplashScreen; // here
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
-import android.os.Bundle; // here
-import org.devio.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +13,17 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
+
    @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this);
@@ -25,14 +36,4 @@ public class MainActivity extends ReactActivity {
   }
 
 
-
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-       return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
-  }
 }
--- android/app/src/main/java/com/narutoarenaapp/MainApplication.java
+++ android/app/src/main/java/com/narutoarenaapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -26,7 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          // packages.add(new SplashScreenReactPackage());
           return packages;
         }
 
