7ef78607cb325d50fd4240af2a8539df5101dfc0
Rodrigo Martins <rodrigo.m.de.campos@accenture.com>
2019-09-24 23:12:15 -0300

    chore: wip page

--- src/screens/Feed/Feed.tsx
+++ src/screens/Feed/Feed.tsx
@@ -1,10 +1,45 @@
 import React from "react";
-import {  Text, View } from "react-native";
+import { View, Image, StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
+import { Colors } from "../../utils";
 
 export const FeedDemo = () => {
   return (
-    <View style={{ backgroundColor: "blue" }}>
-      <Text>fodase</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "space-around",
+        backgroundColor: "#f5f5f5"
+      }}
+    >
+      <Text
+        h1
+        h1Style={{
+          color: Colors.orange,
+          zIndex: 99,
+          textAlign: "center",
+          textShadowColor: "#fff",
+          textShadowOffset: { height: 1, width: 1 },
+          textShadowRadius: 1
+        }}
+      >
+        Work in progress
+      </Text>
+      <View
+        style={[
+          StyleSheet.absoluteFill,
+          { alignSelf: "center", justifyContent: "center" }
+        ]}
+      >
+        <Image
+          source={require("../../../assets/images/naruto_gif.gif")}
+          resizeMode="contain"
+          style={{
+            alignSelf: "center",
+            justifyContent: "center"
+          }}
+        />
+      </View>
     </View>
   );
 };
