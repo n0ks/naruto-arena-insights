ca0693416a69137bcca9aea23342146880e7deea
Rodrigo Martins <rodrigo.m.de.campos@accenture.com>
2019-09-24 23:11:37 -0300

    refactor(nav): change to bottom tab

--- src/navigation/RootStack.tsx
+++ src/navigation/RootStack.tsx
@@ -1,19 +1,19 @@
-import React from "react";
 import {
   createAppContainer,
   createStackNavigator,
-  createDrawerNavigator,
-  DrawerItemsProps
+  createBottomTabNavigator
 } from "react-navigation";
-import { Image } from "react-native-elements";
 import { CharacterDetail, FeedDemo, Characters, ClanLadder } from "../screens";
 
 import { Screens, Colors } from "../utils";
+import { colors } from "react-native-elements";
+import { Image, View } from "react-native";
+import React from "react";
 
 const TAB_ICONS = {
-  [Screens.Characters]: require("../../assets/images/sasuke_sprite.png"),
-  [Screens.Feed]: require("../../assets/images/kisame_sprite.png"),
-  [Screens.ClanLadder]: require("../../assets/images/kakashi_sprite.png")
+  [Screens.Characters]: require("../../assets/images/seal_icon.png"),
+  [Screens.Feed]: require("../../assets/images/konoha_icon.png"),
+  [Screens.ClanLadder]: require("../../assets/images/shisui_sharingan.png")
 };
 
 export const RootStack = createStackNavigator(
@@ -28,29 +28,9 @@ export const RootStack = createStackNavigator(
     },
     [Screens.CharactersDetails]: {
       screen: CharacterDetail,
+
       navigationOptions: {
         title: "Details"
-      }
-    },
-    [Screens.Feed]: {
-      screen: FeedDemo,
-      navigationOptions: {
-        title: "FEED"
-      }
-    },
-    [Screens.ClanLadder]: {
-      screen: ClanLadder,
-      navigationOptions: {
-        drawerIcon: (
-          <Image
-            source={require("../../assets/images/itachi_sprite.png")}
-            borderRadius={32}
-            resizeMode="cover"
-            placeholderStyle={{ borderRadius: 32 }}
-            style={{ width: 32, height: 32 }}
-          />
-        ),
-        title: "Clan Ladder"
       }
     }
   },
@@ -59,86 +39,110 @@ export const RootStack = createStackNavigator(
       headerStyle: {
         backgroundColor: Colors.purple
       },
-      headerTintColor: "#f1f1f1"  
+      headerTintColor: "#f1f1f1"
     }
   }
 );
 
-const DrawerConfigs = createDrawerNavigator(
+const TabNavigatior = createBottomTabNavigator(
   {
     [Screens.Characters]: {
-      screen: RootStack
-      // navigationOptions: {
-      //   drawerIcon: (
-      //     <Image
-      //       source={require("../../assets/images/sasuke_sprite.png")}
-      //       width={75}
-      //       height={75}
-      //       borderRadius={15}
-      //       resizeMode="cover"
-      //       style={{ width: 32, height: 32 }}
-      //     />
-      //   )
-      // }
+      screen: RootStack,
+      title: "Characters",
+      navigationOptions: ({ navigation }) => {
+        console.log(navigation);
+        return {
+          title: "Characters",
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                backgroundColor: focused ? Colors.orange : colors.grey5,
+                borderRadius: 24
+              }}
+            >
+              <Image
+                source={TAB_ICONS[Screens.Characters]}
+                style={{
+                  width: 24,
+                  height: 24
+                }}
+                resizeMethod="resize"
+                resizeMode="contain"
+              />
+            </View>
+          )
+        };
+      }
     },
     [Screens.Feed]: {
-      screen: FeedDemo
-      // navigationOptions: {
-      //   drawerIcon: (
-      //     <Image
-      //       source={require("../../assets/images/kisame_sprite.png")}
-      //       borderRadius={32}
-      //       resizeMode="cover"
-      //       style={{ width: 32, height: 32 }}
-      //     />
-      //   )
-      // }
+      screen: FeedDemo,
+      title: "Feed",
+      navigationOptions: ({ navigation }) => {
+        console.log(navigation);
+        return {
+          title: "Feed",
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                backgroundColor: focused ? Colors.orange : colors.grey5,
+                borderRadius: 24
+              }}
+            >
+              <Image
+                source={TAB_ICONS[Screens.Feed]}
+                style={{
+                  width: 24,
+                  height: 24
+                }}
+                resizeMethod="resize"
+                resizeMode="contain"
+              />
+            </View>
+          )
+        };
+      }
     },
     [Screens.ClanLadder]: {
       screen: ClanLadder,
-      navigationOptions: {
-        drawerIcon: (
-          <Image
-            source={require("../../assets/images/itachi_sprite.png")}
-            borderRadius={32}
-            resizeMode="cover"
-            placeholderStyle={{ borderRadius: 32 }}
-            style={{ width: 32, height: 32 }}
-          />
-        )
+      navigationOptions: navigation => {
+        console.log(navigation);
+        return {
+          title: "Ladders",
+          tabBarIcon: ({ focused }) => (
+            <View
+              style={{
+                backgroundColor: focused ? Colors.orange : colors.grey5,
+                borderRadius: 24
+              }}
+            >
+              <Image
+                source={TAB_ICONS[Screens.ClanLadder]}
+                style={{
+                  width: 24,
+                  height: 24
+                }}
+                resizeMethod="resize"
+                resizeMode="contain"
+              />
+            </View>
+          )
+        };
       }
     }
   },
   {
-    drawerWidth: 300,
-    edgeWidth: 300,
-    drawerPosition: "left",
-    drawerLockMode: "unlocked",
-    drawerType: "back",
-    defaultNavigationOptions: ({ navigation }) => {
-      return {
-        drawerIcon: (
-          <Image
-            source={TAB_ICONS[navigation.state.routeName]}
-            borderRadius={32}
-            resizeMode="cover"
-            style={{ width: 32, height: 32 }}
-          />
-        )
-      };
-    },
-    contentOptions: {
-      // activeTintColor: Colors.orange,
-      // activeBackgroundColor: colors.searchBg,
-      itemsContainerStyle: {
-        marginVertical: 0
-      },
-      iconContainerStyle: {}
-    } as DrawerItemsProps
-    // navigationOptions: ({ navigation }) => {
-    //   console.log("navigation", navigation);
-    // }
+    swipeEnabled: true,
+    initialRouteName: Screens.Characters,
+    tabBarOptions: {
+      // activeBackgroundColor: Colors.orange,
+      activeTintColor: Colors.purple,
+      inactiveTintColor: colors.grey3,
+
+      indicatorStyle: {
+        borderTopColor: Colors.purpleDark
+      }
+    }
   }
 );
 
-export const Drawer = createAppContainer(DrawerConfigs);
+export const TabNav = createAppContainer(TabNavigatior);
