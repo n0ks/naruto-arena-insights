ccc16f36729ce710c35414a024563195d0ec8e85
Rodrigo Martins <rodrigo.m.de.campos@accenture.com>
2019-09-26 01:21:09 -0300

    refactor(component): Chakra

--- src/components/Chakra/Chakra.tsx
+++ src/components/Chakra/Chakra.tsx
@@ -1,20 +1,18 @@
 import React from "react";
 import { View } from "react-native";
 import { chakraMap } from "../../utils";
+import { Icon } from "react-native-elements";
 
 export const Chakra = ({ chakra }) => {
   return (
-    <View
-      style={{
-        width: 14,
-        height: 14,
-        backgroundColor: chakraMap[chakra],
-        marginHorizontal: 4,
-        borderWidth: 1,
-        borderColor: "#f1f1f1",
-        borderRadius: 3,
-        elevation: 1
-      }}
-    />
+    <View>
+      <Icon
+        name="square"
+        type="material-community"
+        containerStyle={{ marginHorizontal: 4 }}
+        size={14}
+        color={chakraMap[chakra]}
+      />
+    </View>
   );
 };
