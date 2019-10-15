9de0c50818fc5ef0f2c8f2c7118909ed8ebeec3f
Rodrigo Martins <rodrigo.m.de.campos@accenture.com>
2019-10-15 16:37:04 -0300

    fix: wrap searchbar in safeareaview

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { SearchBar, colors } from "react-native-elements";
-import { Platform, Animated } from "react-native";
+import { Platform, Animated, SafeAreaView } from "react-native";
 interface Props {
   updateQuery: (text: string) => void;
   queryText: string;
@@ -12,37 +12,38 @@ export const SearchableBar: React.SFC<Props> = ({
   queryText,
   searchY
 }) => {
-
   return (
-    <Animated.View
-      style={{
-        transform: [{ translateY: searchY }],
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        zIndex: 1000,
-      }}
-    >
-      {
-        // @ts-ignore
-        <SearchBar
-          placeholder="Search for ninja name..."
-          onChangeText={updateQuery}
-          containerStyle={{ backgroundColor: "#8240C4" }}
-          placeholderTextColor={colors.greyOutline}
-          inputStyle={{ color: "white" }}
-          clearIcon={{ color: "#fff" }}
-          searchIcon={{ color: "#fff", backgroundColor: "transparent" }}
-          cancelIcon={{ color: "#fff" }}
-          cancelButtonProps={{
-            style: { backgroundColor: "transparent", borderRadius: 20 }
-          }}
-          value={queryText}
-          platform={Platform.select({ ios: "ios", android: "android" })}
-          autoCorrect={false}
-        />
-      }
-    </Animated.View>
+    <SafeAreaView>
+      <Animated.View
+        style={{
+          transform: [{ translateY: searchY }],
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 1000
+        }}
+      >
+        {
+          // @ts-ignore
+          <SearchBar
+            placeholder="Search for ninja name..."
+            onChangeText={updateQuery}
+            containerStyle={{ backgroundColor: "#8240C4",zIndex: 1001 }}
+            placeholderTextColor={colors.greyOutline}
+            inputStyle={{ color: "white" }}
+            clearIcon={{ color: "#fff" }}
+            searchIcon={{ color: "#fff", backgroundColor: "transparent" }}
+            cancelIcon={{ color: "#fff" }}
+            cancelButtonProps={{
+              style: { backgroundColor: "transparent", borderRadius: 20 }
+            }}
+            value={queryText}
+            platform={Platform.select({ ios: "ios", android: "android" })}
+            autoCorrect={false}
+          />
+        }
+      </Animated.View>
+    </SafeAreaView>
   );
 };
