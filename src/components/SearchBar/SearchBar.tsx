f935a7ce339a18eab71cd5f7b564cd60e9e52884
Rodrigo Martins <rodrigo.m.de.campos@accenture.com>
2019-11-19 13:52:59 -0300

    fix: safe area searchbar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -15,47 +15,49 @@ export const SearchableBar: React.SFC<Props> = ({
   searchY,
 }) => {
   return (
-    <Animated.View
-      style={{
-        transform: [{ translateY: searchY }],
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        zIndex: 1000,
-      }}
-    >
-      {
-        // @ts-ignore
-        <SearchBar
-          placeholder="Search for ninja name..."
-          onChangeText={updateQuery}
-          containerStyle={{
-            backgroundColor: '#8240C4',
-            zIndex: 1001,
-          }}
-          placeholderTextColor={colors.greyOutline}
-          inputStyle={{ color: 'white' }}
-          clearIcon={{ color: '#fff' }}
-          searchIcon={{
-            color: '#f5f5f5',
-            backgroundColor: 'transparent',
-          }}
-          cancelIcon={{ color: '#fff' }}
-          cancelButtonProps={{
-            style: {
+    <SafeAreaView style={{ flex: 1, zIndex: 1 }}>
+      <Animated.View
+        style={{
+          transform: [{ translateY: searchY }],
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 1000,
+        }}
+      >
+        {
+          // @ts-ignore
+          <SearchBar
+            placeholder="Search for ninja name..."
+            onChangeText={updateQuery}
+            containerStyle={{
+              backgroundColor: '#8240C4',
+              zIndex: 1001,
+            }}
+            placeholderTextColor={colors.greyOutline}
+            inputStyle={{ color: 'white' }}
+            clearIcon={{ color: '#fff' }}
+            searchIcon={{
+              color: '#f5f5f5',
               backgroundColor: 'transparent',
-              borderRadius: 20,
-            },
-          }}
-          value={queryText}
-          platform={Platform.select({
-            ios: 'ios',
-            android: 'android',
-          })}
-          autoCorrect={false}
-        />
-      }
-    </Animated.View>
+            }}
+            cancelIcon={{ color: '#fff' }}
+            cancelButtonProps={{
+              style: {
+                backgroundColor: 'transparent',
+                borderRadius: 20,
+              },
+            }}
+            value={queryText}
+            platform={Platform.select({
+              ios: 'ios',
+              android: 'android',
+            })}
+            autoCorrect={false}
+          />
+        }
+      </Animated.View>
+    </SafeAreaView>
   );
 };
