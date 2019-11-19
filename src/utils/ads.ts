7e65e319735212f0ca06bf46a154ae5a2be6fd6d
Rodrigo Martins <rodrigo.m.de.campos@accenture.com>
2019-11-19 14:24:00 -0300

    feat: ads with remote config

--- src/utils/ads.ts
+++ src/utils/ads.ts
@@ -1,22 +1,36 @@
 /** @format */
-import { InterstitialAd, AdEventType } from '@react-native-firebase/admob';
+import {
+  InterstitialAd,
+  AdEventType,
+  TestIds,
+} from '@react-native-firebase/admob';
 import chance from 'chance';
+import remoteConfig from '@react-native-firebase/remote-config';
 
-export const loadIntersitialAd = () => {
+export const loadIntersitialAd = async () => {
   const intersitial = InterstitialAd.createForAdRequest(
-    'ca-app-pub-0398075440946116/9116661677',
+    __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-0398075440946116/9116661677',
     {
       requestNonPersonalizedAdsOnly: true,
       keywords: ['anime', 'manga', 'games'],
     }
   );
 
-  if (chance().bool({ likelihood: 33 })) {
-    intersitial.load();
+  intersitial.load();
+
+  try {
+    await remoteConfig().fetchAndActivate();
+
+    const { value } = await remoteConfig().getValue('intersitial_likelihood');
+
     intersitial.onAdEvent((type, _, __) => {
-      if (type === AdEventType.LOADED) {
-        intersitial.show();
+      if (chance().bool({ likelihood: value as number })) {
+        if (type === AdEventType.LOADED) {
+          intersitial.show();
+        }
       }
     });
+  } catch (err) {
+    console.log('err', err);
   }
 };
